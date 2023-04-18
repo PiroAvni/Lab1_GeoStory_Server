2556431c656edfb1f1182b7b946b7552ad7719de
PiroAvni <avnipiro.dev@gmail.com>
2023-04-18 17:58:11 +0100

    adjust the json file facts for all  files

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
-const africa  = require("./Json/africa.json");
-const north_america  = require("./Json/north_america.json");
-const south_america  = require("./Json/south_america.json");
-
+const africa = require("./Json/africa.json");
+const north_america = require("./Json/north_america.json");
+const south_america = require("./Json/south_america.json");
 
 const logger = require("./logger/logger");
 
@@ -14,33 +13,40 @@ app.use(cors());
 app.use(express.json());
 app.use(logger);
 
-
-
+// Home
 app.get("/", (req, res) => {
-    res.send("Welcome to the GeoStory API!");
-  
-  });
+  res.send("Welcome to the GeoStory API!");
+});
 
-  app.get("/africa", (req, res ) => {
-    res.send(africa);
-  
-  });
+// Africa
+app.get("/Africa", (req, res) => {
+  res.send(africa);
+});
 
-  app.get("/random/africa", (req, res) => {
-    const randIdx = Math.floor(Math.random() * africa.length);
-    res.send(africa[randIdx]);
-   
-  });
-  app.get("/random/north_america", (req, res) => {
-    const randIdx = Math.floor(Math.random() * north_america.length);
-    res.send(north_america[randIdx]);
-   
-  });
+app.get("/random/Africa/Random", (req, res) => {
+  const randIdx = Math.floor(Math.random() * africa.length);
+  res.send(africa[randIdx]);
+});
 
-  app.get("/random/south_america", (req, res) => {
-    const randIdx = Math.floor(Math.random() * south_america.length);
-    res.send(south_america[randIdx]);
-   
-  });
- 
+// North Americas
+app.get("/North_Americas/", (req, res) => {
+  const randIdx = Math.floor(Math.random() * north_america.length);
+  res.send(north_america[randIdx]);
+});
+
+app.get("/North_Americas/Random", (req, res) => {
+  const randIdx = Math.floor(Math.random() * north_america.length);
+  res.send(north_america[randIdx]);
+});
+
+// South Americas
+app.get("South_Americas/", (req, res) => {
+  const randIdx = Math.floor(Math.random() * south_america.length);
+  res.send(south_america[randIdx]);
+});
+app.get("South_Americas/Random", (req, res) => {
+  const randIdx = Math.floor(Math.random() * south_america.length);
+  res.send(south_america[randIdx]);
+});
+
 module.exports = app;
